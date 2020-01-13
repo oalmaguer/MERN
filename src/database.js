1532a2b04a6e70db1374b16b29a07835f5a93ba1
oliver alejandro almaguer tostado <almaguero95@gmail.com>
2020-01-12 19:59:56 -0800

    deploy

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://localhost/mern-tasks";
+const URI =
+  "mongodb://oliver:oliver1023@ds135433.mlab.com:35433/heroku_x1pwcx56";
 
 mongoose
-  .connect(URI)
+  .connect(process.env.MONGODB_URI || URI)
   .then(db => console.log("db is connected"))
   .catch(err => console.error(err));
 
